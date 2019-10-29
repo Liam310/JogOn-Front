03819ca2c5a9453a3a6f7265abbf2a945a497ad8
Liam Duncan <liam.duncan310@gmail.com>
2019-10-29 16:15:54 +0000

    ask user for location permissions

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default function App() {
-  return <View style={styles.container}></View>;
+export default class App extends React.Component {
+  state = {
+    errorMessage: null
+  };
+
+  render() {
+    return <View style={styles.container}></View>;
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied'
+      });
+    }
+  };
 }
